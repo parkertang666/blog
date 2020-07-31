010f3f22ba60b07259c7916185a1b3778ad25366
parkertang666 <parkertang666@gmail.com>
2020-07-31 13:15:29 +0800

    OK

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #f5f5f5;
 }
 
 #nav {
@@ -22,7 +22,7 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #666;
 
     &.router-link-exact-active {
       color: #42b983;
